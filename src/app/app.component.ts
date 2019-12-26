93144a4a06445d4d899f436a2b7928a2dd53ccee
Dannee Kim <kim.dannee@gmail.com>
2019-12-26 16:44:39 -0500

    Adding static json file / logging data in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'west-elm-app';
+
+  constructor(
+    public httpService: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.httpService.get('../../assets/data/content.json').subscribe(
+      data => {
+        console.log(data);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
+  }
 }
+
