532006d21e9c5ad3438ec9d5330564ac0325c892
Dannee Kim <kim.dannee@gmail.com>
2019-12-28 16:30:46 -0500

    first hero images displaying

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   navigationItems: Array<any>;
   navigationArray: Array<any>;
+  imagesArray: Array<any>;
 
   constructor(
     public httpService: HttpClient
@@ -19,6 +20,7 @@ export class HomeComponent implements OnInit {
     this.httpService.get('../../assets/data/content.json').subscribe(
       (data: any) => {
         this.navigationArray = [];
+        this.imagesArray = [];
         this.navigationItems = data.groups as any [];
         this.navigationItems.forEach(element => {
           if(element.name.indexOf('&amp')){
@@ -26,8 +28,13 @@ export class HomeComponent implements OnInit {
             linkTitle = element.name.replace(/&amp; Shams/g, '').replace(/&amp;/g, '-');
             element.name = linkTitle;
           }
-          this.navigationArray = this.navigationItems;
+          // if(element.hero){
+          //   console.log(element.hero);
+          //   this.imagesArray.push(element.hero);
+          // }
         })
+        this.navigationArray = this.navigationItems;
+        console.log(this.navigationArray);
         
       },
       (err: HttpErrorResponse) => {
