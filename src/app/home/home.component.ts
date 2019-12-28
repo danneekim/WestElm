eb77dd135e7ae5198ce8df13190402cacd5dde42
Dannee Kim <kim.dannee@gmail.com>
2019-12-28 15:33:05 -0500

    Moving logic to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  navigationItems: Array<any>;
+  navigationArray: Array<any>;
 
-  constructor() { }
+  constructor(
+    public httpService: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.httpService.get('../../assets/data/content.json').subscribe(
+      (data: any) => {
+        this.navigationArray = [];
+        this.navigationItems = data.groups;
+        this.navigationItems.forEach(element => {
+          if(element.name.indexOf('&amp')){
+            let linkTitle = '';
+            linkTitle = element.name.replace(/&amp; Shams/g, '').replace(/&amp;/g, '-');
+            element.name = linkTitle;
+          }
+          this.navigationArray = this.navigationItems;
+        })
+        
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
   }
-
 }
