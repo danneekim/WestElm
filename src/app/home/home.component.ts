8653b1dac82ddaa8dc767732320ffc2f7b0a8259
Dannee Kim <kim.dannee@gmail.com>
2019-12-28 17:10:28 -0500

    Adding alternate hero images to carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+
 })
 export class HomeComponent implements OnInit {
   navigationItems: Array<any>;
