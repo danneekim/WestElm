69709de532ee5d0740c6b00e1cda37e3c79f7683
Dannee Kim <kim.dannee@gmail.com>
2019-12-28 20:40:55 -0500

    Only allowing slide animation on click

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: false, noPause: true, showIndicators: true } }
+  ]
 })
 export class HomeComponent implements OnInit {
   navigationItems: Array<any>;
